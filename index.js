025fab0a5637c686662437fdbd1d72a5ce94362b
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-01-28 12:29:15 -0800

    Add form stuff

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-(function () {})();
+(function () {
+  const FIRM_NAME_ID = '00N30000003ueVA';
+  const FIRM_NAME = 'Robinhood Markets, Inc.';
+  const FIRM_TYPE_ID = '00N30000003ueVF';
+  const FIRM_TYPE = 'Broker-Dealer';
+  const FIRM_SPOKESPERSON_ID = '00N30000003ueVK';
+  const FIRM_SPOKESPERSON = 'Baiju Bhatt';
+  const FIRM_ADDRESS_ID = '00N30000003ueVP';
+  const FIRM_ADDRESS = '';
+  const FIRM_CITY_ID = '00N30000003ueVG';
+  const FIRM_CITY = 'Menlo Park';
+  const FIRM_STATE_ID = 'entityStateCd';
+  const FIRM_STATE = '';
+  const FIRM_ZIP_ID = '00N30000003ueWc';
+  const FIRM_ZIP = '';
+  const FIRM_COUNTRY_ID = 'entityCountryCd';
+  const FIRM_COUNTRY = 'USA';
+})();
